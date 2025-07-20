45e562927a63e143c209efdd09cc79817b19bb74
benjaminpants <maltbyfamily@gmail.com>
2025-07-19 17:35:01 -0700

    i forgot i was in the middle of writing this lol

--- PlusLevelStudio/Patches/SpriteRotatorPatch.cs
+++ PlusLevelStudio/Patches/SpriteRotatorPatch.cs
@@ -1,10 +1,18 @@
-﻿using System;
+﻿using HarmonyLib;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using UnityEngine;
 
 namespace PlusLevelStudio.Patches
 {
+    [HarmonyPatch(typeof(SpriteRotator))]
+    [HarmonyPatch("Update")]
     class SpriteRotatorPatch
     {
+        static bool Prefix(Transform ___cam)
+        {
+            return (___cam != null);
+        }
     }
 }
