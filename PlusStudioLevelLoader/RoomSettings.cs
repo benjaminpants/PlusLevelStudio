3070346f0a971be8e9a99fcf39dcc7976892693e
benjaminpants <maltbyfamily@gmail.com>
2025-08-09 21:00:37 -0700

    nevermind

--- PlusStudioLevelLoader/RoomSettings.cs
+++ PlusStudioLevelLoader/RoomSettings.cs
@@ -14,13 +14,12 @@ namespace PlusStudioLevelLoader
         public StandardDoorMats doorMat;
         public RoomFunctionContainer container;
 
-        public RoomSettings(RoomCategory cat, RoomType type, Color color, StandardDoorMats doors, Material mapMaterial = null, RoomFunctionContainer container = null)
+        public RoomSettings(RoomCategory cat, RoomType type, Color color, StandardDoorMats doors, Material mapMaterial = null)
         {
             category = cat;
             this.type = type;
             this.color = color;
             this.doorMat = doors;
-            this.container = container;
             if (mapMaterial != null)
             {
                 this.mapMaterial = mapMaterial;
