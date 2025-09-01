810e790efd1f523623a0c6aab5261a6248ef4b8b
benjaminpants <maltbyfamily@gmail.com>
2025-09-01 14:13:54 -0700

    yes

---
+++ Lua Examples/basic.lua
@@ -0,0 +1,32 @@
+-- do NOT write any code outside of a function, as it will be executed when the script is initially loaded, and during that process, no global variables are defined yet.
+-- however, defining variables outside a function is a-okay!
+-- A basic example script, containing the bare minimum.
+
+function SetupPlayerProperties()
+	-- not technically necessary to include since these are all the defaults.
+	return {
+		walkSpeed=16,
+		runSpeed=24,
+		staminaDrop=10,
+		staminaMax=100,
+		staminaRise=20
+	}
+end
+
+function Initialize()
+
+end
+
+function ExitedSpawn()
+	self:SpawnNPCs()
+	self:StartEventTimers()
+
+end
+
+function Update(delta)
+
+end
+
+function AllNotebooks()
+	self:OpenExits(true)
+end
