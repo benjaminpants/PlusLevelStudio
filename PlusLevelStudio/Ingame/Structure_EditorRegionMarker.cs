d265924226c0a1d6c88f1f26d69f58acfe300122
benjaminpants <maltbyfamily@gmail.com>
2025-09-04 01:06:42 -0700

    plan for tomorrow

---
+++ PlusLevelStudio/Ingame/Structure_EditorRegionMarker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace PlusLevelStudio.Ingame
+{
+    public class Structure_EditorRegions : StructureBuilder
+    {
+        public override void Load(List<StructureData> data)
+        {
+            base.Load(data);
+            for (int i = 0; i < data.Count; i++)
+            {
+                ec.rooms[data[i].position.x].gameObject.AddComponent<EditorRegionMarker>().region = data[i].position.z;
+            }
+        }
+    }
+
+    public class EditorRegionMarker : MonoBehaviour
+    {
+        public int region;
+    }
+}
